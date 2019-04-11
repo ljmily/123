6f7c85deb77942bdc9ef430db4c86e433c7cbeef
ljmily <victory21ljm@foxmail.com>
2019-04-11 08:59:27 +0800

    first commit

---
+++ src/main/java/xieqi/bawei/com/myapp/MainActivity.java
@@ -0,0 +1,13 @@
+package xieqi.bawei.com.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
